c6db370bd6b37335bcaf34b7aa882d82c85e4c3e
zakidevara <muhammad.devara@traveloka.com>
2025-08-24 01:50:53 +0700

    initial commit

---
+++ flight-springboot/src/test/java/com/example/flight/FlightJpaApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.flight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightJpaApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
